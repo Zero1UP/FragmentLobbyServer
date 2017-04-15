1439ad768b6db32f4dc52956d5756de8fe163273
1UP <zero1up@gmail.com>
2017-04-15 13:17:21 -0400

    Added a LobbyTable Added a Log Table Added a Server Table Added Stored Procedures for dealing with some lookup and insertion of data

--- dotHackFragmentLobbyServer/SQL/fragmentDatabase.sql
+++ dotHackFragmentLobbyServer/SQL/fragmentDatabase.sql
@@ -44,3 +44,64 @@ INSERT INTO Fragment.dbo.Classes VALUES ('Heavy Blade');
 INSERT INTO Fragment.dbo.Classes VALUES ('Heavy Axe');
 INSERT INTO Fragment.dbo.Classes VALUES ('Long Arm');
 INSERT INTO Fragment.dbo.Classes VALUES ('Wavemaster');
+
+
+CREATE Table Fragment.dbo.Logs
+(
+	logID INT IDENTITY(1,1) PRIMARY KEY,
+	logText VARCHAR(2000),
+	logTime DATETIME
+);
+
+
+CREATE Table Fragment.dbo.Lobbies
+(
+	lobbyID INT IDENTITY(1,1) PRIMARY KEY,
+	lobbyName VARCHAR(10)
+)
+
+
+INSERT INTO Fragment.dbo.Lobbies VALUES('Main');
+INSERT INTO Fragment.dbo.Lobbies VALUES('Test');
+
+CREATE Table Fragment.dbo.ServerInfo
+(
+	serverInfoID INT IDENTITY(1,1) PRIMARY KEY,
+	welcomeText VARCHAR(500),
+	serverStatus VARCHAR(400)
+)
+INSERT INTO Fragment.dbo.ServerInfo VALUES('Welcome to the Fragment Test Server!.','Current Status: Barely working!');
+
+/*Stored Procedures */
+USE Fragment;
+GO
+--Stored Procedure for inserting a log
+--These could be anything from debug information, exception logging or just anything that the server sends or receives
+DROP PROCEDURE IF EXISTS InsertLog;
+
+GO
+CREATE PROCEDURE InsertLog   
+    @logText VARCHAR(2000)  
+AS   
+	INSERT INTO Fragment.dbo.Logs (logText,logTime) VALUES (@logText,GETDATE());
+GO
+
+--Stored Procedure for Getting Currently Connected Characters
+DROP PROCEDURE IF EXISTS GetCharacters;
+
+GO
+CREATE PROCEDURE GetCharacters   
+ 
+AS   
+	SELECT * FROM Characters
+GO  
+
+--Stored Procedure for getting the Server Info
+DROP PROCEDURE IF EXISTS GetServerInfo;
+
+GO
+CREATE PROCEDURE GetServerInfo   
+ 
+ AS   
+	SELECT * FROM ServerInfo;
+GO
